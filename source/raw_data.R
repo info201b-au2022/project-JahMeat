0306b0a4957779478419661b3996b38736d5e438
Joshua <joshuaetaylor231@gmail.com>
2022-11-13 02:12:49 -0800

    More observations for raw dataset

--- source/raw_data.R
+++ source/raw_data.R
@@ -4,8 +4,9 @@ library("tidyverse")
 library(readxl)
 nba_raw <- read_excel("data/NBA_raw_data2019.xlsx")
 View(nba_raw)
+# This code is observations from the NBA_raw_data2019 dataset
 
-# Code finding how many people born in 1990 or after have died in the NBA(Joshua)
+# Finding how many people born in 1990 or after have died in the NBA(Joshua) 
 
 death_since_1990 <- nba_raw %>%
   group_by(birthdate, kilos) %>%
@@ -15,9 +16,23 @@ death_since_1990 <- nba_raw %>%
   nrow
   
 
-# finding age of death of average player who has died since the NBA started and 2019(Joshua)
+# Finding age of death of average player who has died since the NBA started and 2019(Joshua)
 
 average_age_death <- nba_raw %>%
   filter(death == "Yes") %>%
   summarise(mean(ageevent)) %>%
   pull()
+
+# Finding how many people died at age 30(Joshua)
+
+under_30 <- nba_raw %>%
+  filter(death == "Yes") %>%
+  filter(ageevent <= 30) %>%
+  summarise(ageevent) %>%
+  nrow
+
+#Finding the percentage of how many people died at age 30 or lower from within dataset(Joshua) 
+
+number_in_dataset <- nba_raw %>%
+  nrow
+percent_under_30 <- sum(under_30 / number_in_dataset)
