b830556fa63b06678986e83a1a11f8561371fcb0
JahMeat <Jah0311@uw.edu>
2022-12-07 13:10:19 -0800

    shiny in NBA folder

--- NBA/app_server.R
+++ NBA/app_server.R
@@ -1,24 +1,56 @@
 # app_server
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
+library(shiny)
+library(tidyverse)
+library(plotly)
+
+background <- p("This project is dedicated on incentivizing health well-being
+                on injuries occuring in the NBA as well as giving additional
+                insights what this could mean for the future of competitive
+                sports. We are asking questions on", em("which types of injuries
+                are the most detrimental to a player's career"), "in order to
+                acknowledge the possible of in game risks. We are also seeking
+                information on", em("how can injuries and player attributes affect
+                their livelihood"), "to bring attention to the current state of their
+                well-being.")
+
+picture <- img(strong("Kobe Bryant Tears Achillies' Tendon, Ending His Season in April 2013!"), 
+               src = "https://static01.nyt.com/images/2013/04/14/sports/subLAKERS/subLAKERS-superJumbo.jpg",
+               width = 500, height = 300
+               )
+
+action <- p("We are tackling these questions by presenting some interative charts
+            on NBA injuries, career status, and livelihoods based on data found on
+            official records from the NBA that has been collected by the data science
+            communities with similar goals as us. These data are utilized to inform
+            medical practioners, NBA associations, and the public like you and me to
+            better understand the seriousness of injuries in competitive sports in order
+            to create better health plans and implement safety guidelines for these 
+            passionate athletes.")
+
+
+
+server <- function(input, output) {
+  output$injuries <- renderPlotly({
+    nba_status <- nba_injuries %>% 
+      group_by(Status, Injuries) %>% 
+      mutate(population = length(Status)) %>% 
+      filter(Status %in% status_frequency == TRUE) %>% 
+      filter(Injuries %in% injury_frequency == TRUE)
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-)
+    injury_plot <- ggplot(data = nba_status) +
+      geom_col(
+        aes(x = Status, y = population, fill = Injuries),
+        position = "dodge"
+      ) +
+      labs( 
+        title = "Most Common NBA Career Status caused by Types of Injuries 2010 - 2018",
+        x = "NBA Career Status",
+        y = "Number of Injuries"
+      )
+    
+    p <- ggplotly(injury_plot)
+    return(p)
+  })
+
+}
--- NBA/app_ui.R
+++ NBA/app_ui.R
@@ -1,17 +1,52 @@
 # app_ui
 
+library(shiny)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
-  })
-}
+# Clean Data
+#----------------------------------------------------
+nba_injuries <- read.csv("../data/injuries.csv")
+
+nba_injuries <- nba_injuries %>% 
+  mutate(Injuries = str_extract(Notes, "\\w+"),
+         Status = str_extract(Notes, "(?<=\\()([^()]*?)(?=\\)[^()]*$)"),
+         year = str_extract(Date, "....")
+  )
+#----------------------------------------------------
+
+page_one <- tabPanel(
+  "Introduction",
+  titlePanel(
+    strong("Injuries in the NBA: 
+            Analyzing the Long Term Effects of Sports Injuries"),
+  ),
+  h4("What's Up?"),
+  background,
+  picture,
+  h4("What's Next?"),
+  action
+)
+
+
+
+page_two <- tabPanel(
+  "Career Injuries",
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    mainPanel(
+      plotlyOutput(outputId = "injuries")
+    )
+  )
+)
+
+
+
+
+ui <- fluidPage(
+  navbarPage(
+    "Stay_Ballin",
+    page_one,
+    page_two
+  )
+)
